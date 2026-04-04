6a4f76618a7e02a9b17903006bebc37c2312a77d
mannansheth <mannan.sheth4@gmail.com>
2026-04-05 00:43:47 +0530

    added .sql file and env example

---
+++ backend/config/db.sql
@@ -0,0 +1,301 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 04, 2026 at 09:11 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `curatus`
+--
+CREATE DATABASE `curatus`;
+USE `curatus`;
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `appointments`
+--
+
+CREATE TABLE `appointments` (
+  `ID` int(11) NOT NULL,
+  `userID` int(11) NOT NULL,
+  `therapistID` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `time` time NOT NULL,
+  `preRemarks` text DEFAULT NULL,
+  `postRemarks` text DEFAULT NULL,
+  `status` enum('upcoming','completed','cancelled','') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `appointments`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `assessment`
+--
+
+CREATE TABLE `assessment` (
+  `ID` int(11) NOT NULL,
+  `userID` int(11) NOT NULL,
+  `data` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `assessment`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `journal_entries`
+--
+
+CREATE TABLE `journal_entries` (
+  `ID` int(11) NOT NULL,
+  `userID` int(11) NOT NULL,
+  `content` text NOT NULL,
+  `createdAt` datetime NOT NULL,
+  `mood` varchar(100) NOT NULL,
+  `score` varchar(100) NOT NULL,
+  `emoji` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `journal_entries`
+--
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `messages`
+--
+
+CREATE TABLE `messages` (
+  `ID` int(11) NOT NULL,
+  `userID` int(11) DEFAULT NULL,
+  `content` text DEFAULT NULL,
+  `ipAddress` varchar(100) DEFAULT NULL,
+  `createdAt` datetime NOT NULL,
+  `isAnonymous` tinyint(4) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `messages`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reactions`
+--
+
+CREATE TABLE `reactions` (
+  `ID` int(11) NOT NULL,
+  `userID` int(11) DEFAULT NULL,
+  `messageID` int(11) NOT NULL,
+  `ipAddress` varchar(20) DEFAULT NULL,
+  `reaction` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `therapists`
+--
+
+CREATE TABLE `therapists` (
+  `id` int(11) NOT NULL,
+  `userID` int(11) NOT NULL,
+  `phone` varchar(20) NOT NULL,
+  `specialization` varchar(255) NOT NULL,
+  `yearsOfExperience` int(11) NOT NULL,
+  `bio` text NOT NULL,
+  `city` varchar(100) NOT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `consultationFee` decimal(10,2) NOT NULL,
+  `mode` enum('online','in-person','both') NOT NULL,
+  `availableDays` varchar(100) NOT NULL,
+  `startTime` time NOT NULL,
+  `endTime` time NOT NULL,
+  `degree` varchar(255) DEFAULT NULL,
+  `certifications` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `ID` int(11) NOT NULL,
+  `Email` varchar(255) NOT NULL,
+  `Name` varchar(255) NOT NULL,
+  `Password` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `appointments`
+--
+ALTER TABLE `appointments`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `userID` (`userID`),
+  ADD KEY `therapistID` (`therapistID`);
+
+--
+-- Indexes for table `assessment`
+--
+ALTER TABLE `assessment`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `userID` (`userID`);
+
+--
+-- Indexes for table `journal_entries`
+--
+ALTER TABLE `journal_entries`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `userID` (`userID`);
+
+--
+-- Indexes for table `messages`
+--
+ALTER TABLE `messages`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `userID` (`userID`);
+
+--
+-- Indexes for table `reactions`
+--
+ALTER TABLE `reactions`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `userID` (`userID`),
+  ADD KEY `messageID` (`messageID`);
+
+--
+-- Indexes for table `therapists`
+--
+ALTER TABLE `therapists`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_therapist_user` (`userID`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `appointments`
+--
+ALTER TABLE `appointments`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `assessment`
+--
+ALTER TABLE `assessment`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `journal_entries`
+--
+ALTER TABLE `journal_entries`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `messages`
+--
+ALTER TABLE `messages`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+
+--
+-- AUTO_INCREMENT for table `reactions`
+--
+ALTER TABLE `reactions`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT for table `therapists`
+--
+ALTER TABLE `therapists`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `appointments`
+--
+ALTER TABLE `appointments`
+  ADD CONSTRAINT `appointments_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `users` (`ID`),
+  ADD CONSTRAINT `appointments_ibfk_2` FOREIGN KEY (`therapistID`) REFERENCES `users` (`ID`);
+
+--
+-- Constraints for table `assessment`
+--
+ALTER TABLE `assessment`
+  ADD CONSTRAINT `assessment_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `users` (`ID`);
+
+--
+-- Constraints for table `journal_entries`
+--
+ALTER TABLE `journal_entries`
+  ADD CONSTRAINT `journal_entries_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `users` (`ID`);
+
+--
+-- Constraints for table `messages`
+--
+ALTER TABLE `messages`
+  ADD CONSTRAINT `messages_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `users` (`ID`);
+
+--
+-- Constraints for table `reactions`
+--
+ALTER TABLE `reactions`
+  ADD CONSTRAINT `reactions_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `users` (`ID`),
+  ADD CONSTRAINT `reactions_ibfk_2` FOREIGN KEY (`messageID`) REFERENCES `messages` (`ID`);
+
+--
+-- Constraints for table `therapists`
+--
+ALTER TABLE `therapists`
+  ADD CONSTRAINT `fk_therapist_user` FOREIGN KEY (`userID`) REFERENCES `users` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
